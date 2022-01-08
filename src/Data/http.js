80a0a994d41f58c6a252f3af234225e4fbb48c54
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-08 09:30:03 +0530

    Update http.js

--- src/Data/http.js
+++ src/Data/http.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://611f24619771bf001785c6fb.mockapi.io/user/";
-
 const methods = {
-  get: axios.get,
-  post: axios.post,
-  delete: axios.delete,
   put: axios.put,
-};
+  post: axios.post,
+  get: axios.get,
+  delete: axios.delete,
+  };
 export default methods;
