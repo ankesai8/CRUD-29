5a5c9c3160470787f0fcb4208e69143fba2c6ccb
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-08 10:00:45 +0530

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,6 +4,10 @@ import React from "react";
 function About() {
   return <p>About Page</p>;
   <p> The Acronym CRUD stands for create, read, update and delete 
+  CREATE procedures: Performs the INSERT statement to create a new record.
+  READ procedures: Reads the table records based on the primary keynoted within the input parameter.
+  UPDATE procedures: Executes an UPDATE statement on the table based on the specified primary key for a record within the WHERE clause of the statement.
+  DELETE procedures: Deletes a specified row in the WHERE clause.
   </p>
   // <div className="grid place-content-center h-screen ">
   //   <div className="grid grid-cols-12 shadow-xl p-4 bg-accent text-dark rounded-md">
